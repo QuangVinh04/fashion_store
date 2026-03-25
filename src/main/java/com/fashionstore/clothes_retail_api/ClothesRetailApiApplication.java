076f2b3e4ae8a0da1d6ceb762beb152bec9687c3
QuangVinh04 <vinhqlnq004@gmail.com>
2026-03-25 16:36:37 +0700

    first commit

---
+++ src/main/java/com/fashionstore/clothes_retail_api/ClothesRetailApiApplication.java
@@ -0,0 +1,13 @@
+package com.fashionstore.clothes_retail_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesRetailApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothesRetailApiApplication.class, args);
+	}
+
+}
