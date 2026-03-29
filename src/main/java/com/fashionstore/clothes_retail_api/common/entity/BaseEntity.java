31d001bfc0a2d1dedc8117877337723a7deb9015
QuangVinh04 <vinhqlnq004@gmail.com>
2026-03-29 22:04:40 +0700

    chore: enable JPA auditing with AuditConfig

--- src/main/java/com/fashionstore/clothes_retail_api/common/entity/BaseEntity.java
+++ src/main/java/com/fashionstore/clothes_retail_api/common/entity/BaseEntity.java
@@ -6,23 +6,39 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     String id;
 
-    @Column(name = "created_at")
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
     LocalDateTime createdAt;
 
+
+    @LastModifiedDate
     @Column(name = "updated_at")
     LocalDateTime updatedAt;
 
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
 
 }
---
+++ src/main/java/com/fashionstore/clothes_retail_api/config/web/AuditConfig.java
@@ -0,0 +1,21 @@
+package com.fashionstore.clothes_retail_api.config.web;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing
+public class AuditConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+
+//        return () -> Optional.ofNullable(SecurityContextHolder.getContext().getAuthentication().getName());
+        return () -> Optional.of("System");
+    }
+}
