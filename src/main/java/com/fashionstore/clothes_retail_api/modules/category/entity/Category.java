4f2d5dcf4ba8993d57158bed8f356af145753baa
QuangVinh04 <vinhqlnq004@gmail.com>
2026-03-29 22:14:09 +0700

    feat(category): add Category entity with parent-child hierarchy

---
+++ src/main/java/com/fashionstore/clothes_retail_api/modules/category/entity/Category.java
@@ -0,0 +1,34 @@
+package com.fashionstore.clothes_retail_api.modules.category.entity;
+
+
+import com.fashionstore.clothes_retail_api.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Category extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    String name;
+
+    @Column(nullable = false, unique = true)
+    String slug;
+
+    String description;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    Category parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    List<Category> children;
+}
