4229401ba2ddd27042b26437616aacc3a5ea749b
QuangVinh04 <vinhqlnq004@gmail.com>
2026-04-10 10:46:31 +0700

    feat(auth): update User entity with roles ManyToMany

---
+++ src/main/java/com/fashionstore/clothes_retail_api/modules/auth/entity/User.java
@@ -0,0 +1,52 @@
+package com.fashionstore.clothes_retail_api.modules.auth.entity;
+
+
+import com.fashionstore.clothes_retail_api.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class User extends BaseEntity {
+
+    @Column(unique = true, nullable = false)
+    String email;
+
+    @Column(nullable = false)
+    String password;
+
+    @Column(nullable = false)
+    String fullName;
+
+    String phone;
+    String address;
+    String avatar;
+
+    @Builder.Default
+    @Column(nullable = false)
+    Boolean isActive = true;
+
+    @Builder.Default
+    @Column(nullable = false)
+    Boolean isEmailVerified = false;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    @Builder.Default
+    Set<Role> roles = new HashSet<>();
+
+}
