c42bbd9be30a50441cf0abbd8a18608dafafc514
QuangVinh04 <vinhqlnq004@gmail.com>
2026-09-13 02:48:26 +0700

    fix(bug): Build payment-service error

--- services/payment-service/src/main/java/com/fashionstore/payment/repository/PaymentRepository.java
+++ services/payment-service/src/main/java/com/fashionstore/payment/repository/PaymentRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PaymentRepository extends JpaRepository<Payment, String> {
     Optional<Payment> findByOrderId(String orderId);
     Optional<Payment> findByTransactionId(String transactionId);
